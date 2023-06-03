294b868df23ef51441d04e14ed6de4def124eb02
Manik <tonysins43@gmail.com>
2023-06-03 19:39:18 +0600

    show costumeShepe Barchart

--- src/Pages/AdminDashboard/AdminHome/AdminHome.jsx
+++ src/Pages/AdminDashboard/AdminHome/AdminHome.jsx
@@ -1,14 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../Hooks/UseAuth";
 import useAxiosSecure from "../../../Hooks/UseAxiosSecure";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
 import {
-  FaMoneyCheck,
-  FaProductHunt,
-  FaTruck,
-  FaUser,
-  FaUsers,
-} from "react-icons/fa";
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
+import { FaMoneyCheck, FaProductHunt, FaTruck, FaUser } from "react-icons/fa";
+import { PieChart, Pie, } from "recharts";
 import "./AdminHome.css";
 
 const AdminHome = () => {
@@ -23,16 +26,63 @@ const AdminHome = () => {
     },
   });
 
-
   // load chart data
+  const { data: chartData = [] } = useQuery({
+    queryKey: ["chart-data"],
+    queryFn: async () => {
+      const res = await axiosSecure("/order-stats");
+      return res.data;
+    },
+  });
 
-  const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+  // barchart
+  const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
+${x + width / 2}, ${y}
+C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+      x + width
+    }, ${y + height}
+Z`;
+  };
 
   const TriangleBar = (props) => {
     const { fill, x, y, width, height } = props;
 
     return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
   };
+
+  // pie chart
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
   return (
     <div className="p-6">
       <h1 className="text-4xl mt-4 md:mt-0 font-semibold">
@@ -84,33 +134,56 @@ const AdminHome = () => {
           </div>
         </div>
       </div>
-      <div className="md:flex">
+      <div className="md:flex mt-12 lg:mt-32">
         <div>
-          <BarChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-              top: 20,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Bar
-              dataKey="uv"
-              fill="#8884d8"
-              shape={<TriangleBar />}
-              label={{ position: "top" }}
+          <ResponsiveContainer width="100%" height={300}>
+            <BarChart
+              data={chartData}
+              margin={{
+                top: 20,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
             >
-              {data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-              ))}
-            </Bar>
-          </BarChart>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="category" />
+              <YAxis />
+              <Bar
+                dataKey="total"
+                fill="#8884d8"
+                shape={<TriangleBar />}
+                label={{ position: "top" }}
+              >
+                {chartData.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                ))}
+              </Bar>
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+        <div>
+          <ResponsiveContainer width="100%" height={300}>
+            <PieChart>
+              <Pie
+                data={chartData}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={80}
+                fill="#8884d8"
+                dataKey="count"
+              >
+                {chartData.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
         </div>
       </div>
     </div>
