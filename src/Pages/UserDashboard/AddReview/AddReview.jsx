1cc2de80ab9356dc4689e2c367d50a582104ab43
Manik <tonysins43@gmail.com>
2023-06-13 00:01:03 +0600

    implement add review

--- src/Pages/UserDashboard/AddReview/AddReview.jsx
+++ src/Pages/UserDashboard/AddReview/AddReview.jsx
@@ -1,7 +1,35 @@
 import { FaMarker } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useAuth from "../../../Hooks/UseAuth";
+import useAxiosSecure from "../../../Hooks/UseAxiosSecure";
+import Swal from "sweetalert2";
 
 const AddReview = () => {
+    const {user} = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+
+    const handleAddReview = (event)=>{
+        event.preventDefault();
+        const form = event.target;
+        const menu = form.menu.value;
+        const rating = form.rating.value;
+        const details = form.description.value;
+        const review = {menu,rating,details,costumerName:user?.displayName};
+        axiosSecure.post('/add-review',review)
+        .then(data=>{
+            if(data.data.insertedId){
+                Swal.fire({
+                    position: 'top-center',
+                    icon: 'success',
+                    title: 'Item successfully Added',
+                    showConfirmButton: false,
+                    timer: 1500
+                  }) 
+            }
+        })
+        
+    }
+
   return (
     <div>
       <SectionTitle
@@ -10,7 +38,7 @@ const AddReview = () => {
       ></SectionTitle>
       <div className="w-10/12 mx-auto bg-[#F3F3F3] px-8  md:px-20 py-12 md:py-16">
         <h3 className="text-3xl font-semibold mb-12 text-center">RATE US!</h3>
-       <form>
+       <form onSubmit={handleAddReview}>
          <div className="form-control w-full ">
           <label className="label">
             <span className="label-text text-xl">
@@ -20,6 +48,7 @@ const AddReview = () => {
           <input
             type="text"
             placeholder="Type here"
+            name="menu"
             className="input input-bordered w-full "
             required
           />
@@ -32,22 +61,24 @@ const AddReview = () => {
             type="number"
             max={5}
             placeholder="Type here"
+            name="rating"
             className="input input-bordered w-full "
             required
           />
         </div>
         <div className="form-control">
           <label className="label">
-            <span className="label-text">Kindly express your care in a short way.</span>
+            <span className="label-text text-xl">Kindly express your care in a short way.</span>
           </label>
           <textarea
             className="textarea textarea-bordered h-36"
             placeholder="Description"
+            name="description"
             required
           ></textarea>
         </div>
 
-        <button className="gradient-btn px-8 py-3 mt-8 text-white font-semibold flex items-center gap-3 rounded-md">Send Review <FaMarker/></button>
+        <button  className="gradient-btn px-8 py-3 mt-8 text-white font-semibold flex items-center gap-3 rounded-md">Send Review <FaMarker/></button>
        </form>
       </div>
     </div>
